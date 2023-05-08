ecb20704e062b60b1450c3e07be161c7adf91266
Nirav Soni <soninirav910@gmail.com>
2023-05-08 18:30:28 +0530

    Added MediatR and AutoMapper

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HR.LeaveManagement.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(o => o.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            return services;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.MappingProfiles
+{
+    public class LeaveTypeProfile : Profile
+    {
+        public LeaveTypeProfile()
+        {
+        }
+    }
+}
