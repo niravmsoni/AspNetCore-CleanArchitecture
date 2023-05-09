8efc3df78543b0f85a3a075a09c2bd3c2e8aae02
Nirav Soni <soninirav910@gmail.com>
2023-05-09 18:26:24 +0530

    Started Infrastructure setup

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
