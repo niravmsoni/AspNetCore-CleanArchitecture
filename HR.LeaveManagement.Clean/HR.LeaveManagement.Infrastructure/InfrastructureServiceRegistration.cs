500c5f453570809da569c976effc6e7f37f63d42
Nirav Soni <soninirav910@gmail.com>
2023-05-10 15:13:58 +0530

    Added code to send emails using sendgrid

--- HR.LeaveManagement.Clean/HR.LeaveManagement.Infrastructure/InfrastructureServiceRegistration.cs
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Models.Email;
+using HR.LeaveManagement.Infrastructure.EmailService;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.ComponentModel.Design;
 
@@ -8,6 +11,10 @@ namespace HR.LeaveManagement.Infrastructure
     {
         public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            //Options pattern - Read from EmailSettings section and convert to strongly typed EmailSettings class
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
+
             return services;
         }
     }
