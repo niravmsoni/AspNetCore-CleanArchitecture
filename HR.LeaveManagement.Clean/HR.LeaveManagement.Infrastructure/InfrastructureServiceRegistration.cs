fdbf745f06f7c688198bec3dc94a05ff2270c7fd
Nirav Soni <soninirav910@gmail.com>
2023-05-10 14:54:47 +0530

    Added Infrastructure class library project

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel.Design;
+
+namespace HR.LeaveManagement.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
